1488c7979d4238f9ea07155eeb271a32a4a0e7f3
USER <USER@192.168.0.100>
2018-03-28 21:32:26 +0900

    RentDAO 구현

--- WebContent/WEB-INF/lib/semi.sql
+++ WebContent/WEB-INF/lib/semi.sql
@@ -2,7 +2,9 @@ create table UserCategory(
 cId number primary key,
 cName varchar2(100) not null
 )
-insert into UserCategory(cId,cName) values(0,'일반회원')
+
+insert into UserCategory(cId,cName) values(0,'일반회원');
+select *from USERCATEGORY;
 
 create table semi_member(
 id varchar2(100) primary key,
@@ -12,8 +14,9 @@ name varchar2(100) not null,
 cId number default 0,
 constraint fk_cId foreign key(cId) references UserCategory(cId)
 )
-select * from semi_member
-drop table semi_member
+
+select * from semi_member;
+drop table semi_member;
 
 create table semi_book(
 bNo number primary key,
@@ -23,6 +26,7 @@ content clob not null,
 publisher varchar2(100) not null,
 isRented number default 0
 )
+select *from SEMI_BOOK
 drop table semi_book
 create sequence semi_book_seq;
 drop sequence semi_book_seq;
@@ -41,30 +45,29 @@ select *from semi_rent_book
 
 
 create table semi_post(
-pNo number primary key,
-title varchar2(100) not null,
-content clob not null,
-hits number default 0,
-
-timeposted date not null,
-id varchar2(100) not null,
-constraint fk_pid foreign key(id) references semi_member(id)
+	pNo number primary key,
+	title varchar2(100) not null,
+	content clob not null,
+	hits number default 0,
+	timeposted date not null,
+	id varchar2(100) not null,
+	constraint fk_pid foreign key(id) references semi_member(id)
 )
-create sequence semi_post_seq
-drop sequence semi_post_seq
-drop table semi_post
+create sequence semi_post_seq;
+drop sequence semi_post_seq;
+drop table semi_post;
+select *from SEMI_POST;
 
 
-
-insert into semi_member(id,password,name,address) values('java','1','아이유','판교')
-insert into semi_member(id,password,name,address) values('spring','1','공유','성남')
+insert into semi_member(id,password,name,address) values('java','1','아이유','판교');
+insert into semi_member(id,password,name,address) values('spring','1','공유','성남');
 select *from semi_member;
 
-insert into semi_book(bNo,title,content,author,publisher,id) values(semi_book_seq.nextval,'반지의제왕','반지이야기','모름','캐나다','spring');
-insert into semi_book(bNo,title,content,author,publisher,id) values(semi_book_seq.nextval,'해리포터','마법사이야기','롤링','영국','spring');
-insert into semi_book(bNo,title,content,author,publisher,id) values(semi_book_seq.nextval,'하얀고양이','고양이가있다','아작','학산','java');
-insert into semi_book(bNo,title,content,author,publisher,id) values(semi_book_seq.nextval,'까만고양이','고양이가없다','아작','학산','java');
-insert into semi_book(bNo,title,content,author,publisher,id) values(semi_book_seq.nextval,'창의력','스케치노트','김충원','우리집','java');
+insert into semi_book(bNo,title,content,author,publisher) values(semi_book_seq.nextval,'반지의제왕','반지이야기','모름','캐나다');
+insert into semi_book(bNo,title,content,author,publisher) values(semi_book_seq.nextval,'해리포터','마법사이야기','롤링','영국');
+insert into semi_book(bNo,title,content,author,publisher) values(semi_book_seq.nextval,'하얀고양이','고양이가있다','아작','학산');
+insert into semi_book(bNo,title,content,author,publisher) values(semi_book_seq.nextval,'까만고양이','고양이가없다','아작','학산');
+insert into semi_book(bNo,title,content,author,publisher) values(semi_book_seq.nextval,'창의력','스케치노트','김충원','우리집');
 select *from semi_book;
 
 insert into semi_post(pNo,title,content,timeposted,id) values(semi_post_seq.nextval,'반지의제왕','반지이야기',sysdate,'spring');
@@ -73,3 +76,8 @@ insert into semi_post(pNo,title,content,timeposted,id) values(semi_post_seq.next
 insert into semi_post(pNo,title,content,timeposted,id) values(semi_post_seq.nextval,'까만고양이','고양이가없다',sysdate'java');
 insert into semi_post(pNo,title,content,timeposted,id) values(semi_post_seq.nextval,'창의력','스케치노트',sysdate,'java');
 select *from semi_post;
+
+insert into SEMI_RENT_BOOK(id,bNo,rentdate,returndate) values('java',1,sysdate,sysdate+7);
+select *from SEMI_RENT_BOOK;
+delete from SEMI_RENT_BOOK where bNo = 1;
+select returndate-sysdate from SEMI_RENT_BOOK where bNo = 1;
